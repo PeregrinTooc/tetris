8f6ef189027ee736bd6c17ea639b3f2ae56a5cf9
Philipp Rembold <philipp.rembold@googlemail.com>
2025-06-07 23:50:33 +0200

    feat: restructure preview container for next tetromino display

--- src/preview-board.js
+++ src/preview-board.js
@@ -1,14 +1,17 @@
 export class PreviewBoard {
     constructor(element) {
         this.element = element;
+        this.previewContainer = element.querySelector("#next-piece").parentElement;
         this.size = 24;
     }
 
     showNextTetromino(tetromino) {
-        this.element.innerHTML = "";
+        this.previewContainer.innerHTML = "";
         const previewElement = tetromino.createElement(document);
+        this.previewContainer.style.position = "relative";
+        previewElement.style.position = "absolute";
         previewElement.style.left = "36px";
         previewElement.style.top = "36px";
-        this.element.appendChild(previewElement);
+        this.previewContainer.appendChild(previewElement);
     }
 }
