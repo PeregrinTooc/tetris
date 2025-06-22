8cc2d351afb318c199d9ad1d1dbc70e382db3c0b
Philipp Rembold <philipp.rembold@sap.com>
2025-06-22 09:07:44 +0200

    test: fix PreviewBoard tests to use correct preview-container DOM structure

--- tests/preview-board.integration.test.js
+++ tests/preview-board.integration.test.js
@@ -2,24 +2,26 @@ import { PreviewBoard } from "../src/preview-board.js";
 import { Tetromino } from "../src/tetromino.js";
 import { Board } from "../src/board.js";
 
-describe("PreviewBoard", () => {
-	let element;
+describe("PreviewBoard integration", () => {
+	let previewElement;
 	let previewBoard;
 	let board;
 	beforeEach(() => {
-		element = document.createElement("div");
+		const container = document.createElement("div");
 		const previewContainer = document.createElement("div");
 		previewContainer.id = "preview-container";
 		const nextPiece = document.createElement("div");
 		nextPiece.id = "next-piece";
 		previewContainer.appendChild(nextPiece);
-		element.appendChild(previewContainer);
-		previewBoard = new PreviewBoard(element);
+		container.appendChild(previewContainer);
+		previewElement = container;
+		previewBoard = new PreviewBoard(previewElement);
 		board = new Board(20, 11, document.createElement("div"), previewBoard);
 	});
-	test("should clear preview container and add tetromino element", () => {
+	test("should show the next tetromino in the preview", () => {
 		const tetromino = Tetromino.createNew(5, document, board);
 		previewBoard.showNextTetromino(tetromino);
-		expect(previewBoard.previewContainer.innerHTML).toContain("tetromino");
+		const previewTetromino = previewBoard.previewContainer.querySelector(".tetromino");
+		expect(previewTetromino).not.toBeNull();
 	});
 });
