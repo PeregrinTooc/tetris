9c92e477b0c7497710b1ea7ec39320c2aaefdd8e
Philipp Rembold <philipp.rembold@sap.com>
2025-09-11 10:44:52 +0200

    stabilize levelling system test

--- cypress/e2e/leveling-system.spec.cy.ts
+++ cypress/e2e/leveling-system.spec.cy.ts
@@ -1,3 +1,7 @@
+
+import { setTetrominoDropTimeInMiliseconds, addTetrominoBase, addTetrominoT, pressLeft, pressRight, pressDown, pressRotate, pressHardDrop, doTimes } from "../support/testUtils";
+
+
 describe("Leveling System", () => {
     beforeEach(() => {
         cy.visit("/");
@@ -9,38 +13,24 @@ describe("Leveling System", () => {
     });
 
     it("should show level increase when scoring enough points", () => {
-        cy.visit("/");
-        cy.get("#start-button").click();
-
-        // Set a fast drop time for testing
         cy.window().then((win) => {
-            win.setTetrominoDropTime(100);
+            doTimes(20, () => addTetrominoBase(win));
         });
+        cy.get("#start-button").click();
 
         // Verify we start at level 1
         cy.get("#level-board").should("contain.text", "Level: 1");
 
         // Score some points by dropping pieces manually to reach level 2 (2000 points)
         // We can use multiple hard drops and score events
-        cy.window().then((win) => {
-            // Push some tetrominos and force score events
-            for (let i = 0; i < 10; i++) {
-                win.pushTetrominoSeed(1337); // Use base tetromino for predictability
-            }
-        });
-
         // Simulate rapid scoring by triggering hard drops
-        for (let i = 0; i < 15; i++) {
-            cy.get("body").type(" "); // Space for hard drop
-            cy.wait(200); // Small delay
-        }
+        doTimes(15, pressHardDrop)
 
         // Check if we reached level 2 eventually
         cy.get("#level-board", { timeout: 10000 }).should("contain.text", "Level: 2");
     });
 
     it("should drop tetrominos faster at higher levels", () => {
-        cy.visit("/");
         cy.get("#start-button").click();
 
         // Set a measurable base drop time
